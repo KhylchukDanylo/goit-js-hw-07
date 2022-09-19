7029cd9c88fbee0339534e352440945554c1cfdb
Danilo <hilcukdanilo@gmail.com>
2022-09-19 22:52:04 +0200

    end work hw

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,43 @@
-import { galleryItems } from './gallery-items.js';
-// Change code below this line
+import { galleryItems } from "./gallery-items.js";
 
-console.log(galleryItems);
+// console.log(galleryItems);
+
+const gallery = document.querySelector(".gallery");
+
+const arrStrImade = [];
+
+function creatSrtElmGallImg(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `<a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>`;
+    })
+    .join("");
+}
+gallery.insertAdjacentHTML("beforeend", creatSrtElmGallImg(galleryItems));
+
+gallery.addEventListener("click", openModalImage);
+function openModalImage(elv) {
+  elv.preventDefault();
+  if (elv.target.nodeName !== "IMG") {
+    return;
+  }
+  const instance = basicLightbox.create(
+    `<img src="${elv.target.dataset.source}" width="800" height="600">`
+  );
+  instance.show();
+
+  if (instance.visible()) {
+    gallery.addEventListener("keydown", (elv) => {
+      if (elv.code === "Escape") {
+        instance.close();
+      }
+    });
+  }
+}
